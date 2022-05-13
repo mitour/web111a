d8898fd907edc11183b452233bda706512db392f
Mitour <love2082020@gmail.com>
2022-05-13 09:35:56 +0800

    0511 homework

--- about.php
+++ about.php
@@ -28,7 +28,16 @@
       </ul>
     </nav>
     <div class="container">
-      <div class="row justify-content-center">
+      <article class="intro">
+        <div class="img">
+          <img src="http://fakeimg.pl/200x200" alt="fake img">
+        </div>
+        <section class="content">
+          <h1>自傳</h1>
+          <p>「您好，我是張碧涵，畢業於中原大學的資訊管理學系，專長是專案管理與網頁前端，對網頁前端擁有極大的熱情，並具備分析、解決問題的能力，以及開放積極的特質。我在大學時，曾在ESIT的網頁部門擔任實習生，負責網頁設計及線上活動執行......。」</p>
+        </section>
+      </article>
+      <!-- <div class="row justify-content-center">
         <section class="col-lg-4 col-md-12 left">
           <h1 class="section_title">About</h1>
           <p class="section_content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Id temporibus laborum voluptates cumque rem, quo quasi, eos blanditiis eveniet ex iusto, nostrum tempora dolorum qui inventore repudiandae deserunt quod velit?</p>
@@ -36,7 +45,7 @@
         <section class="col-lg-4 col-md-12 plain_block">
           <a class="btn btn-primary" href="./index.php"><i class="fa-lg fa-solid fa-house"></i>Home</a>
         </section>
-      </div>
+      </div> -->
     </div>
   </div>
 </body>

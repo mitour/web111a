5c023de3b02b187ffc2d114de259ad92a623e415
Mitour <love2082020@gmail.com>
2022-05-20 09:29:28 +0800

    generate fake image use for loop

--- index.php
+++ index.php
@@ -2,78 +2,17 @@
 
 <div class="css_animation">
   <ul>
-    <li>
-      <img class="img-fluid normal" src="https://source.unsplash.com/random/1" alt="">
-      <div class='info'>
-        <h3>TO</h3>
-      </div>
-    </li>
-    <li>
-      <img class="img-fluid normal" src="https://source.unsplash.com/random/2" alt="">
-      <div class='info'>
-        <h3>BE</h3>
-      </div>
-    </li>
-    <li>
-      <img class="img-fluid normal" src="https://source.unsplash.com/random/3" alt="">
-      <div class='info'>
-        <h3>OR</h3>
-      </div>
-    </li>
-    <li>
-      <img class="img-fluid normal" src="https://source.unsplash.com/random/4" alt="">
-      <div class='info'>
-        <h3>NOT</h3>
-      </div>
-    </li>
-    <li>
-      <img class="img-fluid normal" src="https://source.unsplash.com/random/5" alt="">
-      <div class='info'>
-        <h3>TO</h3>
-      </div>
-    </li>
-    <li>
-      <img class="img-fluid normal" src="https://source.unsplash.com/random/6" alt="">
-      <div class='info'>
-        <h3>BE</h3>
-      </div>
-    </li>
-    <li>
-      <img class="img-fluid normal" src="https://source.unsplash.com/random/7" alt="">
-      <div class='info'>
-        <h3>,</h3>
-      </div>
-    </li>
-    <li>
-      <img class="img-fluid normal" src="https://source.unsplash.com/random/8" alt="">
-      <div class='info'>
-        <h3>IT</h3>
-      </div>
-    </li>
-    <li>
-      <img class="img-fluid normal" src="https://source.unsplash.com/random/9" alt="">
-      <div class='info'>
-        <h3>IS</h3>
-      </div>
-    </li>
-    <li>
-      <img class="img-fluid normal" src="https://source.unsplash.com/random/10" alt="">
-      <div class='info'>
-        <h3>A</h3>
-      </div>
-    </li>
-    <li>
-      <img class="img-fluid normal" src="https://source.unsplash.com/random/11" alt="">
-      <div class='info'>
-        <h3>QUESTION</h3>
-      </div>
-    </li>
-    <li>
-      <img class="img-fluid normal" src="https://source.unsplash.com/random/12" alt="">
-      <div class='info'>
-        <h3><i class="fa-solid fa-exclamation"></i></h3>
-      </div>
-    </li>
+    <?php
+    for ( $i=1 ; $i<=10 ; $i++ ) {
+      $card = '<li>';
+      $card .= '<img class="img-fluid normal" src="https://source.unsplash.com/random/'.$i.'" alt="">';
+      $card .= '<div class="info">';
+      $card .= '<h3>'.$i.'</h3>';
+      $card .= '</div>';
+      $card .= '</li>';
+      echo $card;
+    }
+    ?>
   </ul>
 </div>
 

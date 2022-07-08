76e05421bba0e3ed9812a7a58b14b0f869d84639
Mitour <love2082020@gmail.com>
2022-07-08 09:15:08 +0800

    seperate validation func

--- routes/auth.js
+++ routes/auth.js
@@ -1,24 +1,11 @@
 const router = require("express").Router();
 const User = require("../model/User");
-const Joi = require("joi");
-
-const schema = Joi.object({
-  name: Joi.string().min(3).max(20).required(),
-  email: Joi.string().max(255).required().email(),
-  password: Joi.string()
-    .required()
-    .min(6)
-    .pattern(new RegExp("(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9_]{6,}"))
-    .message(
-      `"password" must be at least one uppercase letter, one lowercase letter and one number`
-    ),
-});
-
+const { registerValidation } = require("../validation");
 router.post(
   "/register",
   async (req, res, next) => {
     try {
-      await schema.validateAsync(req.body);
+      await registerValidation(req.body);
       next();
     } catch (err) {
       res.status(400).send(err.message);
---
+++ validation.js
@@ -0,0 +1,18 @@
+const Joi = require("joi");
+
+const registerValidation = async (data) => {
+  const schema = Joi.object({
+    name: Joi.string().min(3).max(20).required(),
+    email: Joi.string().max(255).required().email(),
+    password: Joi.string()
+      .required()
+      .min(6)
+      .pattern(new RegExp("(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9_]{6,}"))
+      .message(
+        `"password" must be at least one uppercase letter, one lowercase letter and one number`
+      ),
+  });
+  return await schema.validateAsync(data);
+};
+
+module.exports.registerValidation = registerValidation;

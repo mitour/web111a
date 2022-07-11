87fca2f836825788e1b8328613c82f663bd9e7a7
Mitour <love2082020@gmail.com>
2022-07-11 10:12:49 +0800

    change http status code

--- verifyToken.js
+++ verifyToken.js
@@ -2,12 +2,12 @@ const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
   const token = req.header("auth-token");
-  if (!token) return res.status(401).send("Access denied.");
+  if (!token) return res.status(403).send("Access denied.");
   try {
     const verified = jwt.verify(token, process.env.TOKEN_SECRET);
     req.user = verified;
     next();
   } catch (err) {
-    res.status(400).send("invalid token.");
+    res.status(401).send("invalid token.");
   }
 };

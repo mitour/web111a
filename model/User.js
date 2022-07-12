445be6e68563d768e5c5848efa2f8efb48025ea2
Mitour <love2082020@gmail.com>
2022-07-12 09:14:32 +0800

    temporary plan: fix timezoon problem

--- model/User.js
+++ model/User.js
@@ -1,26 +1,31 @@
 const mongoose = require("mongoose");
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    minLength: 3,
-    maxLength: 20,
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      minLength: 3,
+      maxLength: 20,
+    },
+    email: {
+      type: String,
+      required: true,
+      maxLength: 255,
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: 6,
+      maxLength: 1024,
+    },
+    createdAt: { type: Date },
+    updatedAt: { type: Date },
   },
-  email: {
-    type: String,
-    required: true,
-    maxLength: 255,
-  },
-  password: {
-    type: String,
-    required: true,
-    minLength: 6,
-    maxLength: 1024,
-  },
-  createTime: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  {
+    timestamps: {
+      currentTime: () => Date.now() + 60 * 60 * 1000 * 8,
+    },
+  }
+);
 
 module.exports = mongoose.model("User", userSchema);

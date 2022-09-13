f506447ae03a540d5e4f521cb925057d2b37a2ed
Mitour <love2082020@gmail.com>
2022-09-13 23:28:57 +0800

    update hero

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,7 +3,7 @@ import React from "react";
 function Form(props) {
   return (
     <>
-      <div className="hero position-relative">
+      <div className="form-bg position-relative">
         <div className="form_block p-5 pb-3 text-center rounded-4 position-absolute top-50 start-50 translate-middle">
           <h1 className="mb-3 text-black-50 fw-light">{props.type}</h1>
           <form onSubmit={props.handleSubmit}>{props.children}</form>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
     <>
-      <header className="hero"></header>
+      <header className="hero d-flex flex-column justify-content-center align-items-center">
+        <h1 className="text-white">Welcome to We Learn</h1>
+        <Link
+          to="/users/register"
+          className="px-3 btn btn-primary rounded-pill"
+        >
+          Register
+        </Link>
+      </header>
     </>
   );
 }

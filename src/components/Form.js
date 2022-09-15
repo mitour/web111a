2da0e1e1a25869f433f64028a9a5ca73f228dee0
Mitour <love2082020@gmail.com>
2022-09-15 22:59:43 +0800

    fix overflow issue

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,7 +4,7 @@ function Form(props) {
   return (
     <>
       <div className="form-bg position-relative">
-        <div className="form-block p-5 pb-3 text-center rounded-4 position-absolute top-50 start-50 translate-middle">
+        <div className="form-block pb-sm-3 p-sm-5 p-3 pt-5 text-center rounded-4 position-absolute top-50 start-50 translate-middle">
           <h1 className="mb-3 text-black-50 fw-light">{props.type}</h1>
           <form onSubmit={props.handleSubmit}>{props.children}</form>
         </div>

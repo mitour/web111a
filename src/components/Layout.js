276021d6e1aeff06484f71d14b568b4f7728a354
Mitour <love2082020@gmail.com>
2022-09-15 22:54:46 +0800

    update nav-link

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,9 @@
 import React from "react";
+import { useState } from "react";
 import { Link, Outlet } from "react-router-dom";
 
 function Navbar() {
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
   return (
     <>
       <nav className="fixed-top navbar p-sm-0 navbar-expand-sm navbar-dark bg-dark bg-opacity-75">
@@ -29,7 +31,7 @@ function Navbar() {
             className="collapse navbar-collapse justify-content-end"
             id="navbarNav"
           >
-            <ul className="navbar-nav">
+            <ul className="navbar-nav align-items-center">
               <li className="nav-item me-3">
                 <Link className="nav-link px-3" to="/courses">
                   Courses
@@ -40,19 +42,79 @@ function Navbar() {
                   About
                 </Link>
               </li>
-              <li className="nav-item me-3">
-                <Link className="nav-link px-3" to="/users/login">
-                  Login
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link
-                  to="/users/register"
-                  className="nav-link px-3 btn btn-primary rounded-pill"
-                >
-                  Register
-                </Link>
-              </li>
+              {isLoggedIn ? (
+                <>
+                  <li className="nav-item me-3 dropdown">
+                    <Link
+                      class="dropdown-toggle"
+                      to="#"
+                      role="button"
+                      data-bs-toggle="dropdown"
+                      aria-expanded="false"
+                    >
+                      <img
+                        src="https://cdn-icons-png.flaticon.com/512/3006/3006876.png"
+                        className="avatar-sm rounded-circle img-thumbnail d-sm-block d-none"
+                        alt="avatar"
+                      />
+                    </Link>
+                    <Link
+                      class="btn btn-primary dropdown-toggle d-block d-sm-none"
+                      role="button"
+                      to="#"
+                      data-bs-toggle="dropdown"
+                      aria-expanded="false"
+                    >
+                      John doe
+                    </Link>
+                    <ul class="dropdown-menu dropdown-menu-end text-center text-bg-dark mt-2 p-3">
+                      <li className="mb-2">
+                        <img
+                          src="https://cdn-icons-png.flaticon.com/512/3006/3006876.png"
+                          className="avatar-lg rounded-circle img-thumbnail"
+                          alt="avatar"
+                        />
+                      </li>
+                      <li>
+                        <h3 className="h5 m-0">John doe</h3>
+                      </li>
+                      <li>
+                        <small className="text-white-50">Student</small>
+                      </li>
+                      <li className="mt-2">
+                        <Link class="btn btn-primary" to="#">
+                          View Profile
+                        </Link>
+                      </li>
+                      <li className="mt-2">
+                        <Link
+                          className="fw-light text-decoration-none text-light logout-link"
+                          to="/users/login"
+                        >
+                          Logout
+                          <i class="logout"></i>
+                        </Link>
+                      </li>
+                    </ul>
+                  </li>
+                </>
+              ) : (
+                <>
+                  <li className="nav-item me-3">
+                    <Link className="nav-link px-3" to="/users/login">
+                      Login
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link
+                      to="/users/register"
+                      className="nav-link px-3 btn btn-primary rounded-pill"
+                    >
+                      Register
+                    </Link>
+                  </li>
+                </>
+              )}
             </ul>
           </div>
         </div>

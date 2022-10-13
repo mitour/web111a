344a1987d92b0622a4157b4db138a97af691c464
Mitour <love2082020@gmail.com>
2022-10-13 22:35:57 +0800

    UI

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,7 +5,7 @@ function Form(props) {
     <>
       <div className="form-bg position-relative">
         <div className="form-block pb-sm-3 p-sm-5 p-3 pt-5 text-center rounded-4 position-absolute top-50 start-50 translate-middle">
-          <h1 className="mb-3 text-black-50 fw-light">{props.type}</h1>
+          <h1 className="mb-3 text-primary fw-light">{props.type}</h1>
           <form onSubmit={props.handleSubmit}>{props.children}</form>
         </div>
       </div>
